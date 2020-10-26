d25ac56ca1b0cc277cecd472ee2d4c14c26fb6cb
Joe Fowler <scrumplesplunge@gmail.com>
2020-10-26 17:25:39 +0000

    Add a fallback port to main.
    When debugging, the server is restarted many times. If the port is used
    before it is closed, it cannot be used again before a timeout expires,
    so the server cannot be restarted. A fallback port works around this.

--- src/engine.cc
+++ src/engine.cc
@@ -5,8 +5,13 @@
 
 int main() {
   util::http_server server;
-  if (util::status s = server.init({"0.0.0.0", 8000}); s.failure()) {
-    std::cerr << s << '\n';
+  if (util::status s = server.init({"0.0.0.0", 8000}); s.success()) {
+    std::cout << "Serving on 8000\n";
+  } else if (util::status s2 = server.init({"0.0.0.0", 8080}); s2.success()) {
+    std::cout << "Serving on 8080\n";
+  } else {
+    std::cerr << "Failed to bind to 8000 (" << s << ") or 8080 (" << s2
+              << ").\n";
     return 1;
   }
   if (util::status s = server.run(); s.failure()) {

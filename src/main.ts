54a68d3e9e84557a61b75281e17c9386bd4bb65f
Joe Fowler <scrumplesplunge@gmail.com>
2020-11-22 13:13:51 +0000

    Add a canvas.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
+const canvas = document.getElementById('display')! as HTMLCanvasElement;
+const context = canvas.getContext('2d');
+
+function resize() {
+  const r = devicePixelRatio;
+  canvas.width = r * innerWidth;
+  canvas.height = r * innerHeight;
+}
+
+resize();
+addEventListener('resize', resize);
+
 const socket = new WebSocket('ws://localhost:8000/demo');
 
 socket.addEventListener('open', event => {
